7ebf9ec0231370169057761309b56adde7f0a3e1
Precious Olusola <preciousolusola16@gmail.com>
2023-06-01 10:24:40 -0700

    Done with task 4

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_rotate_right - Rotatea binary tree the right.
+ *
+ * @tree: Pointer to the tree root node.
+ *
+ * Return: Pointer to new root node.
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+
+	if (!tree || tree->left == NULL)
+		return (tree);
+
+	new_root = tree->left;
+
+	if (new_root->right)
+	{
+		tree->left = new_root->right;
+		new_root->right->parent = tree;
+	}
+	else
+	{
+		tree->left = NULL;
+	}
+
+	new_root->right = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+
+	return (new_root);
+}
