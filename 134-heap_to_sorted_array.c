c5ca4a3a219ab5e7d6859d8d32164a074935fe04
Precious Olusola <preciousolusola16@gmail.com>
2023-06-02 11:09:41 -0700

    Done with task 134, and i guess that's the anser

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,16 @@
+#include "binary_tree.h"
+
+/**
+ * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
+ *
+ * @heap: Heap to convert to array.
+ * @size: Size of the expected array.
+ *
+ * Return: A sorted array.
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	(void) heap;
+	(void) size;
+	return (0);
+}
