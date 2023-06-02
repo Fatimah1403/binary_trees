d629491c7398ca75789214d2ca4bed5d30bd379c
Precious Olusola <preciousolusola16@gmail.com>
2023-06-02 08:48:38 -0700

    Done with task 112

---
+++ 112-array_to_bst.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - Builds a binary search tree from an array
+ *
+ * @array: Pointer to the first element of the array.
+ * @size: Number of element in the array
+ * Return: Pointer to the root node of the BST.
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *tree;
+	size_t i;
+
+	tree = NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		bst_insert(&tree, array[i]);
+	}
+
+	if (i == size)
+		return (tree);
+
+	return (NULL);
+}
